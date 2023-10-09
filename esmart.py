76cfe4090b7f6c9d7401d61bbfe5d73245b5142f
E-t0m <110770475+E-t0m@users.noreply.github.com>
2023-10-09 11:05:10 +0200

    Update esmart.py
    add some fields for multi device support

--- esmart.py
+++ esmart.py
@@ -13,7 +13,7 @@ REQUEST_MSG0	= b"\xaa\x01\x01\x01\x00\x03\x00\x00\x1e\x32"
 LOAD_OFF		= b"\xaa\x01\x01\x02\x04\x04\x01\x00\xfe\x13\x38"
 LOAD_ON 		= b"\xaa\x01\x01\x02\x04\x04\x01\x00\xfd\x13\x39"
 
-DEVICE_MODE = ["IDLE", "CC", "CV", "FLOAT", "STARTING"]
+DEVICE_MODE = ["IDLE", "CC", "CV", "FLOAT", "START"]
 
 class esmart:
 	def __init__(self):
@@ -23,12 +23,20 @@ class esmart:
 		self.callback = False
 		self.port = ""
 		self.timeout = 0
+		self.name = ""
+		self.ext_temp_name = "bat"
 
 	def __del__(self):
 		self.close()
 
 	def set_callback(self, function):
 		self.callback = function
+
+	def set_name(self, name):
+		self.name = name
+
+	def set_ext_temp_name(self, name):
+		self.ext_temp_name = name
 
 	def open(self, port):
 		self.ser = serial.Serial(port,9600,timeout=0.1)
@@ -44,9 +52,6 @@ class esmart:
 	def send(self, pl):
 		self.ser.write(self.pack(pl))
 
-	def export(self):
-		return(self.fields)
-	
 	def parse(self, data):
 		for c in data:
 			if (self.state == STATE_START):
@@ -77,19 +82,20 @@ class esmart:
 						# Type 0 packet contains most data
 						if (self.data[3] == 0):
 							self.fields = {}
-							self.fields['chg_mode']		= int.from_bytes(self.data[7:9],	byteorder='little')
-							self.fields['pv_volt']		= int.from_bytes(self.data[9:11],	byteorder='little') / 10.0
-							self.fields['bat_volt']		= int.from_bytes(self.data[11:13],	byteorder='little') / 10.0
-							self.fields['chg_cur']		= int.from_bytes(self.data[13:15],	byteorder='little') / 10.0
+							self.fields['chg_mode']	= int.from_bytes(self.data[7:9],	byteorder='little')
+							self.fields['pv_volt']	= int.from_bytes(self.data[9:11],	byteorder='little') / 10.0
+							self.fields['bat_volt']	= int.from_bytes(self.data[11:13],	byteorder='little') / 10.0
+							self.fields['chg_cur']	= int.from_bytes(self.data[13:15],	byteorder='little') / 10.0
 							self.fields['load_volt']	= int.from_bytes(self.data[17:19],	byteorder='little') / 10.0
-							self.fields['load_cur']		= int.from_bytes(self.data[19:21],	byteorder='little') / 10.0
+							self.fields['load_cur']	= int.from_bytes(self.data[19:21],	byteorder='little') / 10.0
 							self.fields['chg_power']	= int.from_bytes(self.data[21:23],	byteorder='little')
-							self.fields['load_power']	= int.from_bytes(self.data[23:25],	byteorder='little')
-							self.fields['bat_temp']		= self.data[25]
-							self.fields['int_temp']		= self.data[27] if self.data[27] < 200 else self.data[27] - 256
+							self.fields['load_power']= int.from_bytes(self.data[23:25],	byteorder='little')
+							self.fields['ext_temp']	= self.data[25]
+							self.fields['int_temp']	= self.data[27] if self.data[27] < 200 else self.data[27] - 256
 							self.fields['soc']		= self.data[29]
-							self.fields['co2_gram']		= int.from_bytes(self.data[33:35], byteorder='little')
-
+							self.fields['co2_gram']	= int.from_bytes(self.data[33:35], byteorder='little')
+							self.fields['name']		= self.name
+							self.fields['ext_temp_name']	= self.ext_temp_name
 							self.callback(self.fields)
 
 	def tick(self):
